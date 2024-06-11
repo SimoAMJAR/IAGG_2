ad4841e4a54da99c9568f4c91d5ebd580ce51bf6
SimoAMJAR <simoo.amjar@gmail.com>
2024-06-11 09:26:00 +0200

    fixing the levels

--- levels.py
+++ levels.py
@@ -11,14 +11,14 @@ class Levels:
     def __init__(self):
         self.current_level = 1
         self.levels = {
-            1: Level(1, 5, 4, 0),  # Level 1: 5 knives, rotation speed 1, 3 preplaced knives
-            2: Level(2, 5, 4, 2),  
-            3: Level(3, 7, 4, 2),  
-            4: Level(4, 5, 4, 4),  
-            5: Level(5, 5, 4, 2),  
-            6: Level(6, 5, 4, 5),  
-            7: Level(7, 5, 4, 8),  
-            8: Level(8, 5, 4, 10)
+            1: Level(1, 5, 3, 0),  # Level 1: 5 knives, rotation speed 1, 3 preplaced knives
+            2: Level(2, 5, 3, 2),  
+            3: Level(3, 7, 5, 0),  
+            4: Level(4, 5, 5, 3),  
+            5: Level(5, 7, 5, 3),  
+            6: Level(6, 7, 6, 2),  
+            7: Level(7, 3, 5, 8),  
+            8: Level(8, 2, 5, 10)
         }
 
     def get_current_level(self):
